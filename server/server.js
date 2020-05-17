e652e11d9f8a8f173bf41a6c53e2cb275e92ddbf
serviolee <serviolee@gmail.com>
2020-05-16 17:59:06 -0700

    rendering my component from aws

--- server/server.js
+++ server/server.js
@@ -2,9 +2,20 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8000;
+const cors = require('cors');
+const axios = require('axios');
 
 app.use(express.static(path.join(__dirname, '../client')));
-console.log(__dirname);
+app.use(cors());
+
+app.get('/restaurants/:restaurant_id/photos', (req, res) => {
+  console.log(req.params);
+  axios.get(`http://18.218.219.141:3001/restaurants/${req.params.restaurant_id}/photos`)
+    .then((result) => {
+      console.log(result.data);
+      res.send(result.data);
+    })
+})
 
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
