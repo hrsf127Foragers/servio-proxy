c9f1c519e86c1bd3b39f41960b8efb96ee08e136
serviolee <serviolee@gmail.com>
2020-05-16 23:40:25 -0700

    rendering all components

--- server/server.js
+++ server/server.js
@@ -9,13 +9,55 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(cors());
 
 app.get('/restaurants/:restaurant_id/photos', (req, res) => {
-  console.log(req.params);
   axios.get(`http://18.218.219.141:3001/restaurants/${req.params.restaurant_id}/photos`)
     .then((result) => {
-      console.log(result.data);
       res.send(result.data);
     })
+    .catch((err) => {
+      console.log(err);
+    })
 })
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  axios.get(`http://18.218.219.141:3001/restaurants/${req.params.restaurant_id}`)
+    .then((result) => {
+      res.send(result.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
+app.get('/api/:restaurant_id/reviews', (req, res) => {
+  axios.get(`http://54.183.220.133:3001/api/${req.params.restaurant_id}/reviews`)
+    .then((result) => {
+      res.send(result.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
+app.get('/:collection_id/restaurants', (req, res) => {
+  axios.get(`http://13.57.246.172/${req.params.collection_id}/restaurants`)
+    .then((result) => {
+      res.send(result.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
+app.get('/:restaurant_id/collections', (req, res) => {
+  axios.get(`http://13.57.246.172/${req.params.restaurant_id}/collections`)
+    .then((result) => {
+      res.send(result.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
